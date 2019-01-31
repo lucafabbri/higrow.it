1d3d889b86b798955c3ce423c40bf07b9a39735b
Luca Fabbri <luca@zepfiro.com>
2019-01-31 14:20:42 +0100

    analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-51910998-52 ',
+  checkDuplicatedScript: true
+})
 
 
 new Vue({
